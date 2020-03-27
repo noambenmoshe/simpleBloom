4f7d1980fd2c36a855632407bf4b85a98ec2e170
noambenmoshe <b.noam@campus.technion.ac.il>
2020-03-27 14:55:45 +0300

    get functions added

--- src/Simulation.java
+++ src/Simulation.java
@@ -3,6 +3,23 @@ public class Simulation {
     private int sizeOfS;
     private int sizeOfSample; //number of numbers to check if exists
     private boolean[] universeVector; // will be true if the index is inside our set
+    private int falsePositive;
+
+    public int getFalsePositive() {
+        return falsePositive;
+    }
+
+    public int getN() {
+        return n;
+    }
+
+    public int getSizeOfS() {
+        return sizeOfS;
+    }
+
+    public int getSizeOfSample() {
+        return sizeOfSample;
+    }
 
     public Simulation(int universeSize, int setSize, int sampleSize){
         n = universeSize;
@@ -10,4 +27,10 @@ public class Simulation {
         sizeOfSample = sampleSize;
         universeVector = new boolean[n];
     }
+
+    void AddToUniverse(int randNum){
+        universeVector[randNum]=true;
+    }
+
+
 }
