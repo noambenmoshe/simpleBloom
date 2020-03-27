5f5029bcaad32075db3ac7f7c00661d3437330a8
noa.cohen@campus.technion.ac.il <onTHEm00n>
2020-03-27 15:21:28 +0300

    some main

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,43 @@
+import java.util.Random;
+//import java.Integer.parseInt()
+
 public class Main {
 
+    void SetRandomizer(Simulation sim, Bloom bloomFilter){
+        Random randClass = new Random();
+        for(int i = 0; i < sim.getSizeOfS(); i++){
+            int rand = randClass.nextInt(sim.getN());
+            sim.AddToUniverse(rand);
+            bloomFilter.Add(rand);
+        }
+    }
 
+    void SampleRandomizer(Simulation sim, Bloom bloomFilter){
+        Random randClass = new Random();
+        for(int i = 0; i < sim.getSizeOfSample(); i++){
+            int rand = randClass.nextInt(sim.getN());
+            boolean inSet = sim.checkIfInSet(rand);
+            boolean bloomAns = bloomFilter.checkIfExists(rand);
+            if (bloomAns && !inSet)
+                sim.falsePositiveCounterIncrement();
+        }
+    }
 
     public static void main(String[] args) {
-        Bloom b1= new Bloom(10,3);
+        // interpreting input arguments
+        int uniSize = Integer.parseInt(args[1]);
+        int setSize = Integer.parseInt(args[2]);
+        int sampleSize = Integer.parseInt(args[3]);
+        int numOfSampleRuns = Integer.parseInt(args[4]);
+        int bloomVecLength = Integer.parseInt(args[5]);
+        int numOfHashFuncs = Integer.parseInt(args[6]);
 
+        // initializing bloom filter & simulation
+        Bloom b1= new Bloom(bloomVecLength,numOfHashFuncs);
+        Simulation simulation= new Simulation(uniSize, setSize, sampleSize);
+
+        //
+        
         System.out.println("Hello World");
     }
 }
--- src/Simulation.java
+++ src/Simulation.java
@@ -1,6 +1,6 @@
 public class Simulation {
     private int n; //universe size
-    private int sizeOfS;
+    private int sizeOfS; // S is the set size
     private int sizeOfSample; //number of numbers to check if exists
     private boolean[] universeVector; // will be true if the index is inside our set
     private int falsePositiveCounter;
