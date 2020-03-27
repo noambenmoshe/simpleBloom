3c2b1258a9d098974d78d03b1194587ca8270036
noambenmoshe <b.noam@campus.technion.ac.il>
2020-03-27 13:36:32 +0300

    members in class

--- src/Bloom.java
+++ src/Bloom.java
@@ -1,6 +1,11 @@
+
 public class Bloom {
     // please work
+    private int m; //filter length
+    private int k; //number of hash functions
+    private int[] bloomArray;
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
 }
+
