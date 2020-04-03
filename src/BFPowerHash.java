549890239183459d42a3596f322f671f469ea21b
noa.cohen@campus.technion.ac.il <onTHEm00n>
2020-04-03 14:06:37 +0300

    BFpowerhash

--- src/BFPowerHash.java
+++ src/BFPowerHash.java
@@ -1,3 +1,31 @@
-public class BFPowerHash implements BloomFilter{
+import java.lang.Math;
+import java.util.ArrayList;
+import java.util.Collections;
 
+public class BFPowerHash implements BloomFilter{
+    private int m; //filter length
+    private int k; //number of hash functions
+    private ArrayList<Boolean> bloomArray;
+
+    public BFPowerHash(int length, int numOfFunc){
+        m = length;
+        k = numOfFunc;
+        bloomArray = new ArrayList<>();
+        //initialize bloomArray
+        Collections.fill(bloomArray, Boolean.FALSE);
+    }
+
+    public void insert(int number){
+        for (int i=1; i<=k; i++){
+            bloomArray.add((int)Math.pow(number,i)%m,Boolean.TRUE);
+        }
+    }
+
+    public boolean search(int number){
+        for (int i=1; i<=k; i++){
+            if (!bloomArray.get((int)Math.pow(number,i)%m))
+                return false;
+        }
+        return true;
+    }
 }
