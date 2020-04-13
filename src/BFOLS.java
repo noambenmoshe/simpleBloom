1646aedab9eb689c4e90a41421d30eaeb13b520b
noambenmoshe <b.noam@campus.technion.ac.il>
2020-04-13 12:07:43 +0300

    BFOLS with descriptions

--- src/BFOLS.java
+++ src/BFOLS.java
@@ -1,6 +1,7 @@
 
 import java.util.ArrayList;
 import java.util.Map;
+import java.util.Vector;
 
 public class BFOLS implements BloomFilter {
     private int m; //filter length
@@ -8,6 +9,7 @@ public class BFOLS implements BloomFilter {
     private Map<Integer, Boolean> bloomFilter;
     private HashFunction hashFunction;
 
+    // regular constructor
     public BFOLS(int m, int k, Map<Integer, Boolean> bloom_Filter, HashFunction hash_Function ) {
         this.m = m;
         this.k = k;
@@ -15,17 +17,31 @@ public class BFOLS implements BloomFilter {
         this.hashFunction = hash_Function;
     }
 
+    //inputs: s - represents the square root of the universe size
+    //        d - represents the free zone size  / size of set you can insert with out false positives
+    //        hashFunction - represents each number and it's mask bits
+    public BFOLS(int s, int d, HashFunction hashFunction){
+        this.m = s * (d + 1);
+        this.k = d + 1;
+        this.hashFunction = hashFunction;
+    }
+
     @Override
+    // gets vector that says which bits need to be true in the bloomFilter,
+    // then changes each relevant bit to be true in bloomFilter
     public void insert(int number) {
-        ArrayList<Integer> bit_mask = hashFunction.get(number);
+        Vector<Integer> bit_mask = hashFunction.get(number);
         for (int bit : bit_mask){
             bloomFilter.put(bit,true);
         }
     }
 
     @Override
+    // gets vector that says which bits need to be true in the bloomFilter,
+    // then if all relevant bits are true in bloomFilter then number is found in bloomFilter return true
+    // otherwise number is not found, return false
     public boolean search(int number) {
-        ArrayList<Integer> bit_mask = hashFunction.get(number);
+        Vector<Integer> bit_mask = hashFunction.get(number);
         for (int bit : bit_mask){
             if(!bloomFilter.get(bit)){
                 return false;
