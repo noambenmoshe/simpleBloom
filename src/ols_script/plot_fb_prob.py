49df1b535d779bd53e10581a59caafa6c6d2b933
noa.cohen@campus.technion.ac.il <onTHEm00n>
2020-11-14 15:37:53 +0200

    python files updated

--- src/ols_script/plot_fb_prob.py
+++ src/ols_script/plot_fb_prob.py
@@ -3,8 +3,8 @@ from scipy.optimize import curve_fit
 import numpy as np
 
 
-def probability_fit(x, d, a, b, c):  # , d):
-    return d * pow(x, 4) + a * pow(x, 3) + b * pow(x, 2) + c * x  # + d
+def probability_fit(x, a, b, c):  # , d):
+    return a * pow(x, 3) + b * pow(x, 2) + c * x  # + d
 
 
 def addToDict(dictionary, d, setSize, fpPercent):
@@ -18,7 +18,7 @@ def addToDict(dictionary, d, setSize, fpPercent):
 
 def createDictionary(type):
     dictionary = {}
-    with open(f'output_for_graphs_{type}_FP.txt') as fp:
+    with open(f'output_for_graphs_{type}_FP_01.txt') as fp:
         Lines = fp.read().splitlines()
         bfType = Lines.pop(0)
         assert(type == bfType)
@@ -44,25 +44,23 @@ def createDictionary(type):
 def plotVecSizeByUniSize(d):
     fig = go.Figure()
     # POL
-    fig.add_trace(
-        go.Scatter(x=pol_dict[d]['setSizeList'], y=pol_dict[d]['fpPercent'], name='POL', mode='markers',
-                   line=dict(color='firebrick')))
+    fig.add_trace(go.Scatter(x=pol_dict[d]['setSizeList'], y=pol_dict[d]['fpPercent'], name='POL', mode='markers',
+                             line=dict(color='firebrick')))
     popt, pcov = curve_fit(probability_fit, pol_dict[d]['setSizeList'], pol_dict[d]['fpPercent'])
     y_fit_vals = probability_fit(np.array(pol_dict[d]['setSizeList']), *popt)
     fig.add_trace(go.Scatter(x=pol_dict[d]['setSizeList'], y=y_fit_vals, line_shape='spline',
                   line=dict(color='firebrick', width=2), name='POL fit'))
     # OLS
-    fig.add_trace(
-        go.Scatter(x=ols_dict[d]['setSizeList'], y=ols_dict[d]['fpPercent'], name='OLS', mode='markers',
-                   line=dict(color='royalblue')))
+    fig.add_trace(go.Scatter(x=ols_dict[d]['setSizeList'], y=ols_dict[d]['fpPercent'], name='OLS', mode='markers',
+                             line=dict(color='royalblue')))
     popt, pcov = curve_fit(probability_fit, ols_dict[d]['setSizeList'], ols_dict[d]['fpPercent'])
     y_fit_vals = probability_fit(np.array(ols_dict[d]['setSizeList']), *popt)
-    fig.add_trace(go.Scatter(x=ols_dict[d]['setSizeList'], y=y_fit_vals, line_shape='spline',
-                  line=dict(color='royalblue', width=2, dash='dash'), name='OLS fit'))
+    fig.add_trace(go.Scatter(x=pol_dict[d]['setSizeList'], y=y_fit_vals, line_shape='spline',
+                             line=dict(color='royalblue', width=2, dash='dash'), name='OLS fit'))
     # TITLE
-    fig.update_layout(title=f'Universe Elements False Positive Percentage',
+    fig.update_layout(title=f'False Positive Probability',
                       xaxis_title=r"$\text{Set Size}$",
-                      yaxis_title=r"$\text{Percentage}$")
+                      yaxis_title=r"$\text{Probability}$")
     fig.show()
 
 
